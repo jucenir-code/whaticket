e41914d1f364eb577ec5885f11a1070970d32f10
Jucenir <jucenir@jcsistemas.cloud>
2024-08-15 14:39:48 -0300

    first commit

---
+++ frontend/build/precache-manifest.113b45f6620e14e50ed17a0ed987a515.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5df7b57871db38398b3bd82187ba26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccd189dd1acf5e3c0d55",
+    "url": "/static/css/2.406c6332.chunk.css"
+  },
+  {
+    "revision": "41be0e8feece4da92ed6",
+    "url": "/static/css/main.40de1c4b.chunk.css"
+  },
+  {
+    "revision": "ccd189dd1acf5e3c0d55",
+    "url": "/static/js/2.f572a2ca.chunk.js"
+  },
+  {
+    "revision": "719da88b2b4a1c7abe7d92d39bd84ca6",
+    "url": "/static/js/2.f572a2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b25b33a966d34b17373",
+    "url": "/static/js/3.87e25722.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.87e25722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41be0e8feece4da92ed6",
+    "url": "/static/js/main.b3951883.chunk.js"
+  },
+  {
+    "revision": "0a443a4e9b91d4582280",
+    "url": "/static/js/runtime-main.1869f4bf.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
